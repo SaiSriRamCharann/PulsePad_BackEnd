8b5c10d56aa2d4d6532484491a486434ce734b68
SaiSriRamCharann <saisriramcharanpindi@gmail.com>
2025-09-27 23:02:10 +0530

    Deploying backend

--- src/config/DBconnect.js
+++ src/config/DBconnect.js
@@ -1,8 +1,16 @@
-const mongoose= require("mongoose")
+const mongoose = require("mongoose");
 
-const DBconnect=async()=>{
-    await mongoose.connect("mongodb+srv://ramnaidu_db_user:qcQYQnszvYfGoYGg@projectcluster.ztjia9f.mongodb.net/PulsePad");
-    
-}
+const DBconnect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Database Connected Successfully");
+    } catch (err) {
+        console.error("Database Connection Failed:", err);
+        process.exit(1); 
+    }
+};
 
-module.exports=DBconnect;
+module.exports = DBconnect;
